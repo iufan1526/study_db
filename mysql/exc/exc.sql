36884ee35df1f683a273a6f65df4516a29a6137a
iufan1526 <iufan1526@gmail.com>
2022-01-24 11:53:37 +0900

    123

---
+++ mysql/exc/exc.sql
@@ -0,0 +1,32 @@
+use world;
+
+show tables;
+
+
+create table mamber (
+
+	seq int,
+    name varchar(100),
+	id varchar(100),
+    regdata datetime
+
+);
+
+
+create table mamber2 (
+
+	seq int,
+    name varchar(100),
+	id varchar(100),
+    regdata datetime
+
+);
+
+
+
+
+
+
+
+
+
